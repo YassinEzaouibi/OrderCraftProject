99d3c2441858a0de080acec4b81aad2be24c8fce
YassinXXXX03 <122600366+YassinXXXX03@users.noreply.github.com>
2023-12-24 16:07:54 +0100

    Orders Classses

---
+++ src/main/java/ordercraft/project/dao/UserDao.java
@@ -0,0 +1,17 @@
+package ordercraft.project.dao;
+
+import ordercraft.project.DataBase.ConnectDataBase;
+import ordercraft.project.dao.Interfaces.IOrder;
+import ordercraft.project.model.Order;
+import ordercraft.project.model.Product;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+
+}
